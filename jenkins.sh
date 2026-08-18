9d8f33fa2f9cb8c800e7c20f0afd108080836394
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-18 09:39:17 +0530

    jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -12,7 +12,7 @@ xfs_growfs /var/tmp
 xfs_growfs /var
 
 
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-21-openjdk -y
 yum install jenkins -y
