7343db4f81dfa1fd76f7dc75ff1fd249be55a8fe
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-21 11:45:31 +0530

    jenkins

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -34,3 +34,13 @@ usermod -aG docker ec2-user
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+eksctl version
+kubectl version
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
